616c48e9de1ff7f89d521e56bfba6abaed5760cb
Tim Sanders <tsanders@bluefin.com>
2020-01-17 08:12:59 -0500

    Added debugging information to cardconex_fetch.rb

--- cardconex_fetch.js
+++ cardconex_fetch.js
@@ -19,6 +19,7 @@ var mySqlConnection = mysql.createConnection({host: 'localhost', user: 'tsanders
 //  *   Add the table to the alaSQL JOIN
 
 //  These are the queries to populate the in memory-tables from Sales Force; includes no logic.
+//  VERY IMPORTANT!  FROM in the SQL statements below must be in all capital letters.
 var objQueries={
 	 "Account":'SELECT AccountNumber,Site,AccountSource,Account_Status__c,Active_ACH_IDs__c,Active_Acquiring_IDs__c,Active_Encryption_IDs__c,Active_Gateway_IDs__c,Active_Services__c,Additional_Documentation_Needed__c,BillingCity,BillingCountry,BillingCountryCode,BillingPostalCode,BillingState,BillingStateCode,BillingStreet,Billing_Contact_Email_Addresses__c,Business_Start_Date__c,CreatedById,CreatedDate,Customer_Number__c,DBA_Name__c,Description,Exemption_No__c,Id,Industry,IsPartner,Is_501c3__c,LastActivityDate,LastModifiedById,LastModifiedDate,Lead_Type__c,Legacy_ID__c,Legacy_Source__c,Max_Contract_End_Date__c,Months_In_Business__c,Name,NumberOfEmployees,Open_Opportunities__c,Open_Opportunity_Amount__c,OrganizationID__c,OwnerId,ParentId,Partnership_Type__c,Partner_Service_Instructions__c,Phone,PhotoUrl,RecordTypeId,Revenue_Segment__c,ShippingAddress,ShippingCity,ShippingCountry,ShippingCountryCode,ShippingGeocodeAccuracy,ShippingLatitude,ShippingLongitude,ShippingPostalCode,ShippingState,ShippingStateCode,ShippingStreet,Sic,SicDesc,Type,Unique_Count__c,Website FROM Account'
 	,"Bank_Account__c":'SELECT Bank_Name__c,ABA__c,AccountId__c,Account_Type__c,ACH_Debit_Account_Name__c,ACH_Descriptor__c,Bank_Account_Number__c,Bank_Account_Type__c,Bank_City__c,Bank_Contact_Name__c,Bank_Phone__c,Bank_Postal_Code__c,Bank_State__c,Bank_Street_Address__c,Business_Person_Named_on_teh_account__c,CreatedById,CreatedDate,Id,LastModifiedDate,Name FROM Bank_Account__c'
@@ -29,7 +30,26 @@ var objQueries={
 	,"User":'SELECT Id,Name,FirstName,LastName FROM User'
 };
 
-var arrTables=['Account'];  // Object.keys(objQueries);
+
+var tableNameParameter = 'Account';     // later, pass this variable as a command-line paramter.
+console.log('\r\nd1 tableNameParameter = ' + tableNameParameter);
+var arrTables = [];
+
+arrTables.push(tableNameParameter);
+console.log('\r\nd2 arrTables = ' + arrTables);
+// convert the following to a function 
+var fieldList;
+var insertStatement;
+
+if(arrTables[0] == 'Account'){
+    fieldList = objQueries.Account.substring(7, objQueries.Account.indexOf(' FROM'));
+    console.log('\r\nd3 fieldList = ' + fieldList);
+
+    insertStatement = 'INSERT INTO '.concat(arrTables[0]).concat('(').concat(fieldList).concat(') VALUES ?');
+    console.log('\r\nd4 insertStatement = ' + insertStatement);
+}else{
+    console.log('debug 1 something with with table name');
+}
 
 var objData={};
 
@@ -40,71 +60,25 @@ const currentTimestamp = function(){
 
 const fnSync=function(){
     
-	var go=true;
 	for(var i=0;i<arrTables.length;i++){
 		if(!objData[arrTables[i]] || objData[arrTables[i]].length === 0){ 
 			console.log('Waiting on records for ' + arrTables[i] + '...' );
 			return false;
 		}
 	}
-                   
-    var strQuery = 'SELECT AccountNumber,Site,AccountSource,Account_Status__c,Active_ACH_IDs__c,Active_Acquiring_IDs__c,Active_Encryption_IDs__c,Active_Gateway_IDs__c,Active_Services__c,Additional_Documentation_Needed__c,BillingCity,BillingCountry,BillingCountryCode,BillingPostalCode,BillingState,BillingStateCode,BillingStreet,Billing_Contact_Email_Addresses__c,Business_Start_Date__c,CreatedById,CreatedDate,Customer_Number__c,DBA_Name__c,Description,Exemption_No__c,Id,Industry,IsPartner,Is_501c3__c,LastActivityDate,LastModifiedById,LastModifiedDate,Lead_Type__c,Legacy_ID__c,Legacy_Source__c,Max_Contract_End_Date__c,Months_In_Business__c,Name,NumberOfEmployees,Open_Opportunities__c,Open_Opportunity_Amount__c,OrganizationID__c,OwnerId,ParentId,Partnership_Type__c,Partner_Service_Instructions__c,Phone,PhotoUrl,RecordTypeId,Revenue_Segment__c,ShippingAddress,ShippingCity,ShippingCountry,ShippingCountryCode,ShippingGeocodeAccuracy,ShippingLatitude,ShippingLongitude,ShippingPostalCode,ShippingState,ShippingStateCode,ShippingStreet,Sic,SicDesc,Type,Unique_Count__c,Website FROM ? AS account';
 
-	var arrOutput = alasql(strQuery,[objData.Account]);
-    
-    var arrRows = [];
-    
-    // Write the output file.
-	fnSave(arrOutput,'sfBilling.csv');
-    
+    var strQuery = objQueries.Account.replace(" FROM ", " FROM ? ");    // adjust the SQL statement.
+    console.log('\r\nd5 strQuery = ' + strQuery);
+
+	var arrOutput = alasql(strQuery,[objData.Account]);                 // execute the SQL statement and store the result set in an array of objects, one for each row.              
+    console.log('\r\nd6 [objData.Account] = ' + [objData.Account]);
+
+    /* convert the result set from an array of objects to an array of rows that fnInsertRows can use to update the database. */
+    var arrRows = [];          
     for (var i=0; i<arrOutput.length; i++){
         arrRows[i] = Object.values(arrOutput[i]);
     }
- 
-    // console.log(arrRows[0]);
-    
-    fnInsert(arrRows, 'account');
- 
-    // Create a .CSV file that has the contents of the other tables - for debugging only.
-
-    /*
-    console.log('Exporting all tables for debugging...\n');
-    
-    strQueryAccount                              = 'SELECT * FROM ? AS account';
-    var arrOutputAccount                         = alasql(strQueryAccount,[objData.Account]);
-    fnSave(arrOutputAccount,'sfAccount.csv');
-    
-    strQueryAsset                                = 'SELECT AccountId,AssetLevel,ContractId__c,CreatedById,CreatedDate,Description,Fee_Amount_Text__c,Fee_Amount__c,Fee_Group__c,Fee_Name__c,Id,Identification_NumberId__c,ID_Number_Text__c,InstallDate,LastModifiedById,LastModifiedDate,Name,OwnerId,ParentId,Price,Product2Id,ProductCode,PurchaseDate,Quantity,Quote_Line_ItemId__c,SerialNumber,Status,UsageEndDate FROM ? AS asset';
-    var arrOutputAsset                           = alasql(strQueryAsset,[objData.Asset]);
-    // console.log(arrOutputAsset);
-    fnSave(arrOutputAsset,'sfAsset.csv');
-    
-    strQueryBankAccount                          = 'SELECT * FROM ? AS bankAccount';
-    var arrOutputBankAccount                     = alasql(strQueryBankAccount,[objData.Bank_Account__c]);
-    fnSave(arrOutputBankAccount,'sfBankAccount.csv');    
-
-    strQueryContract                             = 'SELECT * FROM ? AS contract';
-    var arrOutputContract                        = alasql(strQueryContract,[objData.Contract]);
-    fnSave(arrOutputContract,'sfContract.csv'); 
-    
-    strQueryIdentificationNumber                 = 'SELECT * FROM ? AS identificationNumber';
-    var arrOutputIdentificationNumber            = alasql(strQueryIdentificationNumber,[objData.Identification_Number__c]);
-    fnSave(arrOutputIdentificationNumber,'sfBankIdentificationNumber.csv');  
- 
-    strQueryRecordType                           = 'SELECT * FROM ? AS recordType';
-    var arrOutputRecordType                      = alasql(strQueryRecordType,[objData.RecordType]);
-    fnSave(arrOutputRecordType,'sfRecordType.csv'); 
-    
-    strQueryUser                                 = 'SELECT * FROM ? AS user';
-    var arrOutputUser                            = alasql(strQueryUser,[objData.User]);
-    fnSave(arrOutputUser,'sfUser.csv'); 
-
-
-    strQueryAsset                                = 'SELECT * FROM ? AS asset';
-    var arrOutputAsset                           = alasql(strQueryAsset,[objData.Asset]);
-    fnSave(arrOutputAsset,'sfAsset.csv');
-    */
-
+    fnInsertRows(arrRows, 'account');
 }
 
 const fnQuery=function(strTable){
@@ -161,7 +135,7 @@ const fnSave=function(arrData,strFile){
 
 
 
-const fnInsert=function(arrRecords, tableName){
+const fnInsertRows=function(arrRecords, tableName){
 /*
     PURPOSE:  INSERT rows into the database.
     
@@ -170,15 +144,8 @@ const fnInsert=function(arrRecords, tableName){
 */
 
 	console.log('Number for rows to add to the database:  ' + arrRecords.length);
+    console.log('d2 = ' + tableName);
 
-    // console.log(arrRecords);
-
-    var sqlInsertAccount = 
-        `INSERT INTO account(AccountNumber,Site,AccountSource,Account_Status__c,Active_ACH_IDs__c,Active_Acquiring_IDs__c,Active_Encryption_IDs__c,Active_Gateway_IDs__c,Active_Services__c,Additional_Documentation_Needed__c,BillingCity,BillingCountry,BillingCountryCode,BillingPostalCode,BillingState,BillingStateCode,BillingStreet,Billing_Contact_Email_Addresses__c,Business_Start_Date__c,CreatedById,CreatedDate,Customer_Number__c,DBA_Name__c,Description,Exemption_No__c,Id,Industry,IsPartner,Is_501c3__c,LastActivityDate,LastModifiedById,LastModifiedDate,Lead_Type__c,Legacy_ID__c,Legacy_Source__c,Max_Contract_End_Date__c,Months_In_Business__c,Name,NumberOfEmployees,Open_Opportunities__c,Open_Opportunity_Amount__c,OrganizationID__c,OwnerId,ParentId,Partnership_Type__c,Partner_Service_Instructions__c,Phone,PhotoUrl,RecordTypeId,Revenue_Segment__c,ShippingAddress,ShippingCity,ShippingCountry,ShippingCountryCode,ShippingGeocodeAccuracy,ShippingLatitude,ShippingLongitude,ShippingPostalCode,ShippingState,ShippingStateCode,ShippingStreet,Sic,SicDesc,Type,Unique_Count__c,Website
-         ) VALUES ?`;
-        
-/*    var sqlInsertAccount = 'INSERT INTO ' + tableName + '(acct_id) VALUES ?';*/
-    
     mySqlConnection.connect(function(err) {
         if (err){
             console.log('Could not connect to MySQL.\n');
@@ -186,7 +153,7 @@ const fnInsert=function(arrRecords, tableName){
         } else{
             console.log('Successfully connected to MySQL.\n');
 
-            mySqlConnection.query(sqlInsertAccount, [arrRecords], function(err, results, fields){
+            mySqlConnection.query(insertStatement, [arrRecords], function(err, results, fields){
             if(err){
                 console.log('MySQL ERROR ' + err.errno + ': ' + err.code + '\n' + err.sqlMessage);
             }else{
@@ -202,17 +169,15 @@ const fnInsert=function(arrRecords, tableName){
 
 }
 
-// Log in to Sales Force
-sfConnect.login(config.sf.user, config.sf.password, function(objLoginError, objLoginResponse) {
-  if (objLoginError) { 
-    return console.error(objLoginError); 
-  }
-  // YOU CANNOT COMPARE FIELDS DIRECTLY IN THIS LANGUAGE, MUST USE SUB SELECTS :(
-  // SO WE WILL JUST GRAB EACH TABLE WORTH OF INFO AND JOIN WITH ALASQL AND QUERY 
+/* main */
+sfConnect.login(config.sf.user, config.sf.password, function(objLoginError, objLoginResponse){
+    if (objLoginError){ 
+        return console.error(objLoginError); 
+    }
   
-// import tables to memory.
-  for(var i=0; i<arrTables.length; i++){ 
-    fnQuery(arrTables[i]); 
-  }
+    // import table  
+    for(var i=0; i<arrTables.length; i++){ 
+        fnQuery(arrTables[i]); 
+    }
   
 });
